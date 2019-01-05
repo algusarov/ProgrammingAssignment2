3725479153f61282d306ae8b9cc5263b23590085
algusarov <algusarov@pm.me>
2019-01-05 21:16:30 +0300

    Submission of Coursera programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCasheMatrix creates a wrapper around matrix passed
+## to this function as x, additional functionality added is caching
+## of matrix inverse, use function set to initialize or change the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    cachedInverse <- NULL
 
+    set <- function(y) {
+        x <<- y
+        cachedInverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) cachedInverse <<- inverse
+    getinverse <- function() cachedInverse
+
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse
+    )
 }
 
 
-## Write a short comment describing this function
+## Function which complements matrix caching wrapper, this function
+## calculates inverse matrix only if it was not calculated before
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("Getting cached data")
+        return(inv)
+    }
+    inv <- solve(x$get(), ...)
+    x$setinverse(inv)
+    inv
 }
